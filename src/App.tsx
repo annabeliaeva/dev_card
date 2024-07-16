fffb80dfad2bcc1262cd3c6f5ea427ad673a6893
anna_beliaeva <anna_beliaeva@list.ru>
2024-07-16 17:11:39 +0300

    feat: add Search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { Container } from 'components/Container'
+<<<<<<< HEAD
+=======
+import { Search } from 'components/Search'
+>>>>>>> ec15b77 (feat: add Search component)
 import { TheHeader } from 'components/TheHeader'
 
 function App() {
   return (
     <Container>
       <TheHeader />
+<<<<<<< HEAD
+=======
+      <Search hasError={false} onSubmit={() => {}} />
+>>>>>>> ec15b77 (feat: add Search component)
     </Container>
   )
 }
